c3f9dc1c551bef9a1a0d6141ebbc32c405007932
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-16 13:18:58 -0700

    add list of functions commentary

--- functions.js
+++ functions.js
@@ -1,1 +1,25 @@
-// Your functions go here!
+// Implement the filter Method on a Prototype
+
+// Return Part of an Array Using the slice Method
+
+// Remove Elements from an Array Using slice Instead of splice
+
+// Combine Two Arrays Using the concat Method
+
+// Add Elements to the End of an Array Using concat Instead of push
+
+// Use the reduce Method to Analyze Data
+
+// Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+
+// Sort an Array Alphabetically using the sort Method
+
+// Split a String into an Array Using the split Method
+
+// Combine an Array into a String Using the join Method
+
+// Apply Functional Programming to Convert Strings to URL Slugs
+
+// Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
